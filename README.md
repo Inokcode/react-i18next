944726108b797cfe52261df634947738fa8d2113
Inokcode <regilearn@gmail.com>
2021-11-01 10:39:29 +0530

    readme url added

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Getting Started with Create React App
 
+i18next
+react-i18next
+https://dev.to/adrai/how-to-properly-internationalize-a-react-application-using-i18next-3hdb
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -1,13 +1,83 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'flag-icon-css/css/flag-icons.min.css';
+
+import App from './App';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'fr', 'ar'],
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    // resources: {
+    //   en: {
+    //     translation: {
+    //       'Welcome to React': 'Welcome to React and react-i18next',
+    //     },
+    //   },
+    //   fr: {
+    //     translation: {
+    //       'Welcome to React': 'Welcome to React and react-i18next french',
+    //     },
+    //   },
+    // },
+    // lng: 'en', // if you're using a language detector, do not define the lng option.
+    // lng: document.querySelector('html').lang,
+    fallbackLng: 'en',
+    detection: {
+      order: [
+        'path',
+        'cookie',
+        'htmlTag',
+        'querystring',
+        'localStorage',
+        'sessionStorage',
+        'navigator',
+        'subdomain',
+      ],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    // react: { useSuspense: false },//it try to show some placeholders//to make true use suspence to wrap the app,used lodingmarkup for this
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
+
+// function App() {
+//   const { t } = useTranslation();
+
+//   return <h2>{t('welcome_to_React')}</h2>;
+// }
+
+const loadingMarkup = (
+  <div className='"py-4 text-center'>
+    <h2>Loading...</h2>
+  </div>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
 
